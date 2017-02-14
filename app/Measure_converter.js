9198a5b50b4ec4b5e1692f48a52b14b78bdc0568
Hernandez Julien <julienhernandez14@gmail.com>
2017-02-14 20:14:21 +0100

    First Commit
    Init app

---
+++ app/Measure_converter.js
@@ -0,0 +1,7 @@
+exports.unitsMetricToImperial = function(units){
+	console.log('Metrique vers imperial'+units);
+};
+
+exports.unitsImperialToMetric = function(units){
+    console.log('Imperial vers metrique'+units);
+};
---
+++ app/index.js
@@ -0,0 +1,34 @@
+// app/index.js
+
+var http = require('http') ;
+var url = require('url') ;
+var Measure_converter = require('./Measure_converter') ;
+
+var server = http.createServer(function (requete, resultat) {
+  var page = url.parse(requete.url).pathname ;
+  var sortie ;
+  var data;
+  resultat.writeHead(200,{"Content-Type": "text/plain; charset=UTF-8"});
+   
+	// See if distance is Metrics or Imperial
+    let km = page.indexOf('km');
+    let m = page.indexOf('m');
+    let cm = page.indexOf('cm');
+    let inch = page.indexOf('in');
+    let foot = page.indexOf('ft');
+    let yard = page.indexOf('ya');
+    let miles = page.indexOf('mi');
+
+    console.log(km);
+    if (km >= 0 || m >= 0 || cm >= 0) {
+        data = Measure_converter.unitsMetricToImperial(page);
+    } else if (miles >= 0 || yard >= 0 || foot >= 0 || inch>= 0) {
+        data = Measure_converter.unitsImperialToMetric(page);
+    } else {
+        data = 'Erreur de distance !!!';
+    }
+    
+  resultat.end(data);
+});
+
+server.listen(8888) ;
